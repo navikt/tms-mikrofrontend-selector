1f1660985e393ac57402c78bd2c9b897af50aaf9
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2025-02-14 10:00:40 +0100

    Hente digisos kun fra GCP da de har migrert fra fss

--- app/src/test/kotlin/no/nav/tms/mikrofrontend/selector/ApiTest.kt
+++ app/src/test/kotlin/no/nav/tms/mikrofrontend/selector/ApiTest.kt
@@ -495,32 +495,6 @@ internal class ApiTest {
 
         }
     }
-    @Test
-    fun `Retunere sosialhjelp produktkort dersom digisos gir respons fra FSS men ikke GCP `() = testApplication {
-
-        initSelectorApi(testident = "12345678910")
-        initExternalServices(
-            SafRoute(),
-            MeldekortRoute(),
-            OppfolgingRoute(),
-            PdlRoute(),
-            DigisosRoute(),
-            LegacyDigisosRoute(true),
-            )
-
-
-        client.get("/din-oversikt").assert {
-            status shouldBe HttpStatusCode.OK
-            objectMapper.readTree(bodyAsText()).assert {
-                this["microfrontends"].size() shouldBe 0
-                this["produktkort"].toList().assert {
-                    size shouldBe 1
-                    first().asText() shouldBe "KOM"
-                }
-            }
-
-        }
-    }
 
     fun ApplicationTestBuilder.initSelectorApi(
         testident: String,
