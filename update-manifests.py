65ef6b1988adc43b1e0488574b88aa939ba93359
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2025-06-06 18:36:27 +0200

    Migrer til nytt manifest

--- update-manifests.py
+++ update-manifests.py
@@ -2,6 +2,7 @@
 import json
 import sys
 json_file_name = 'manifests.json'
+json_file_name_v2 = 'manifests-v2.json'
 
 # Opening JSON file
 with open(json_file_name, 'r') as json_file:
@@ -12,3 +13,19 @@ with open(json_file_name, 'r') as json_file:
 
 with open(json_file_name,'w') as json_out_file:
     json_out_file.write(out_json)
+
+# Opening JSON file
+with open(json_file_name_v2, 'r') as json_file_v2:
+    data_v2 = json.load(json_file_v2)
+    data_v2[sys.argv[1]] = {
+        'url': sys.argv[2],
+        'appname': '',
+        'namespace': '',
+        'fallback': '',
+        'ssr': False
+    }
+    out_json_v2 = json.dumps(data_v2)
+    json_file_v2.close()
+
+with open(json_file_name_v2, 'w') as json_out_file_v2:
+    json_out_file_v2.write(out_json_v2)
