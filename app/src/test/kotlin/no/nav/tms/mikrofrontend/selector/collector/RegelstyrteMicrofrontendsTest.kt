63118fdc1145d730f2da6116fd1dbd23b7c66d7b
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2025-06-23 14:51:51 +0200

    Parse nytt manifest

--- app/src/test/kotlin/no/nav/tms/mikrofrontend/selector/collector/RegelstyrteMicrofrontendsTest.kt
+++ app/src/test/kotlin/no/nav/tms/mikrofrontend/selector/collector/RegelstyrteMicrofrontendsTest.kt
@@ -3,21 +3,24 @@ package no.nav.tms.mikrofrontend.selector.collector
 import io.kotest.matchers.shouldBe
 import no.nav.tms.mikrofrontend.selector.collector.regelmotor.ContentDefinition
 import no.nav.tms.mikrofrontend.selector.safTestDokument
+import no.nav.tms.mikrofrontend.selector.versions.Discovery
+import no.nav.tms.mikrofrontend.selector.versions.DiscoveryManifest
 import no.nav.tms.token.support.tokenx.validation.LevelOfAssurance
 import org.junit.jupiter.api.Test
 
 class RegelstyrteMicrofrontendsTest {
-    private val manifestMapWithPensjon =
-        mapOf("pensjonskalkulator-microfrontend" to "https://cdn.pensjon/manifest.json")
+    private val discoveryManifestWithPensjon = DiscoveryManifest(mapOf("pensjonskalkulator-microfrontend" to Discovery(
+        "https://cdn.pensjon/manifest.json", "name", "pensjon", "http://pensjon/fallback", true
+    )))
 
     @Test
     fun `pensjon skal vises hvis personen er over 40 år og ikke har sakstema pensjon`() {
-        ContentDefinition.getAktueltContent(41, listOf("DAG".safTestDokument()), manifestMapWithPensjon, LevelOfAssurance.SUBSTANTIAL).run {
+        ContentDefinition.getAktueltContent(41, listOf("DAG".safTestDokument()), discoveryManifestWithPensjon, LevelOfAssurance.SUBSTANTIAL).run {
             size shouldBe 1
             first().id shouldBe "pensjonskalkulator-microfrontend"
             first().url shouldBe "https://cdn.pensjon/manifest.json"
         }
-        ContentDefinition.getAktueltContent(41, listOf("DAG".safTestDokument()), manifestMapWithPensjon, LevelOfAssurance.HIGH).run {
+        ContentDefinition.getAktueltContent(41, listOf("DAG".safTestDokument()), discoveryManifestWithPensjon, LevelOfAssurance.HIGH).run {
             size shouldBe 1
             first().id shouldBe "pensjonskalkulator-microfrontend"
             first().url shouldBe "https://cdn.pensjon/manifest.json"
@@ -25,26 +28,26 @@ class RegelstyrteMicrofrontendsTest {
         ContentDefinition.getAktueltContent(
             alder = 41,
             listOf("PEN".safTestDokument()),
-            manifestMapWithPensjon,
+            discoveryManifestWithPensjon,
             LevelOfAssurance.HIGH
         ).size shouldBe 0
         ContentDefinition.getAktueltContent(
             alder = 39,
             listOf("FOR".safTestDokument(), "DAG".safTestDokument()),
-            manifestMapWithPensjon,
+            discoveryManifestWithPensjon,
             levelOfAssurance = LevelOfAssurance.HIGH
         ).size shouldBe 0
         ContentDefinition.getAktueltContent(
             alder = 39,
             listOf("PEN".safTestDokument()),
-            manifestMapWithPensjon,
+            discoveryManifestWithPensjon,
             levelOfAssurance = LevelOfAssurance.SUBSTANTIAL
         ).size shouldBe 0
 
         ContentDefinition.getAktueltContent(
             alder = 39,
             listOf(),
-            manifestMapWithPensjon,
+            discoveryManifestWithPensjon,
             levelOfAssurance = LevelOfAssurance.HIGH
         ).size shouldBe 0
     }
